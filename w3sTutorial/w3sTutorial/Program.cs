fd96c766d2849b9565aebb60bf7f733b35e2a3f1
namtong <namtong@omt.com.vn>
2020-03-05 11:40:37 +0700

    C# HOME C# Intro C# Get Started C# Syntax C# Comments C# Variables C# Data Types C# Type Casting C# User Input C# Operators C# Math

---
+++ w3sTutorial/w3sTutorial/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace w3sTutorial
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            //Console.WriteLine("Hello World");
+            string myName = "NamTong";
+            const int myNumb = 36;
+            const float myNumbFloat = 36.9F;
+            const bool myBool = true;
+            int x = 1, y = 2, z = 3, xx = 4;
+            int yy = -3;
+
+  //          Console.WriteLine(x += 5);
+            Console.WriteLine(Math.Max(x, y));
+            Console.WriteLine(Math.Min(x, y));
+            Console.WriteLine(Math.Sqrt(y));
+            Console.WriteLine(Math.Abs(yy));
+            Console.WriteLine(Math.Round(myNumbFloat));
+
+            Console.WriteLine("Hello " + $"{myName} {myNumb} {myNumbFloat} {myBool}");
+            
+            Console.WriteLine(myNumb + myNumbFloat);
+            
+            Console.WriteLine(x + y + z);
+
+            Console.WriteLine(Convert.ToDouble(myNumb));
+
+            Console.WriteLine(Convert.ToInt32(myNumbFloat));
+            
+            Console.WriteLine(Convert.ToString(myNumb));
+
+            Console.WriteLine("Hello World");
+            String username = Console.ReadLine();
+            Console.WriteLine("my name is : " + username);
+
+
+            Console.WriteLine("Enter your age: " + 3);
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("My age: " + age);
+
+
+        }
+    }
+}
